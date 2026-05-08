35c0061fc9a6fd2fcc941e226e9789b4c8d476d9
deepak-rana-du <deepak.rana@dusol.ac.in>
2026-05-08 08:13:54 +0530

    Hello Program in Kotlin

---
+++ src/Basic.kt
@@ -0,0 +1,8 @@
+/*
+1. The first line of program defines a function called main(). In Kotlin, function is a group of statements that performs a group of tasks. Functions start with a keyword fun followed by function name (main in this case).
+
+The main () function takes an array of string (Array<String>) as a parameter and returns Unit. Unit is used to indicate the function and does not return any value (void as in Java). Declaring Unit is an optional, we do not declare it explicitly.
+ */
+fun main(args:Array <String>){
+    println("Hello Kotlin")
+}
