a1f5c1a6cd6c90a774cdfb3033afa76ede8fb0e0
deepak <deepak9313@gmail.com>
2026-05-10 22:44:24 +0530

    Operator in Kotlin

---
+++ src/Operator.kt
@@ -0,0 +1,24 @@
+/*
+Operator in Kotlin
+Operators are special characters which perform operation on operands (values or variable).There are various kind of operators available in Kotlin.
+
+Arithmetic operator         +,-,*,/,%
+Relation operator           >,<,>=,<=,==,!=
+Assignment operator         !=,-=,,*=,/=,%=
+Unary operator
+
++           unary plus                  +a          a.unaryPlus()
+-           unary minus                 -a          a.unaryMinus()
+++          increment by 1              ++a         a.inc()
+--          decrement by 1              --a         a.dec()
+!           not                         !a          a.not()
+
+Bitwise operation
+Logical operator    && , || , !
+ */
+fun main(args:Array<String>){
+    var a = 10
+    var b = 20
+    var c = 30
+    println(a+b+c)
+}
